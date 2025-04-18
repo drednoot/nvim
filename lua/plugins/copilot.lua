189a0d1b3a7712f4535a435ddae41700af453503
ns <matveyerohin228@gmail.com>
2025-04-19 00:12:34 +0300

    feat: add copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,31 @@
+return {
+  'github/copilot.vim',
+  enabled = true, 
+  lazy = false,
+
+  config = function()
+    vim.g.copilot_proxy = os.getenv("COPILOT_PROXY_URL")
+    vim.g.copilot_no_tab_map = true
+    vim.g.copilot_assume_mapped = true
+    vim.g.copilot_filetypes = {
+      ['gitcommit'] = false,
+      ['gitrebase'] = false,
+      ['svn'] = false,
+      ['help'] = false,
+      ['TelescopePrompt'] = false,
+      ['oil'] = false,
+    }
+  end,
+
+  keys = {
+    {
+			'<c-l>',
+      mode = 'i',
+			function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), 'i')
+      end,
+      noremap = true,
+      silent = true,
+		},
+  }
+}
